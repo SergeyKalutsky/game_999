ac4fd3ffe4de71610d8301b6723bc33ddf953fee
SergeyKalutsky <skalutsky@gmail.com>
2021-07-17 17:18:39 +0300

    made a board

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame as pg
 
 WIDTH, HEIGHT = 600, 600
 WHITE = (255, 255, 255)
-GREY = (50, 50, 50)
+GREY = (127, 127, 127)
 FPS = 30
 
 pg.init()
@@ -11,12 +11,29 @@ screen = pg.display.set_mode((WIDTH, HEIGHT))
 clock = pg.time.Clock()
 pg.display.set_caption('ШАШКИ!')
 
+
+def pick_color(i, j):
+    if i % 2 == 0:
+        if j % 2 == 0:
+            return WHITE
+        else:
+            return GREY
+    else:
+        if j % 2 == 0:
+            return GREY
+        else:
+            return WHITE
+
+
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
 
-    pg.draw.rect(screen, WHITE, pg.Rect(30, 30, 60, 60))
+    for i in range(8):
+        for j in range(8):
+            color = pick_color(i, j)
+            pg.draw.rect(screen, color, pg.Rect(20+i*70, 20+j*70, 70, 70))
+
     pg.display.update()
     clock.tick(FPS)
-        
